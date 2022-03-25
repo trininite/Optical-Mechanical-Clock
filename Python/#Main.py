b1f4c800ec71ce589c359ced37145694b752dc38
killerbox04 <killerbox04@gmail.com>
2022-03-25 12:21:40 -0400

    Output side lables

--- Python/#Main.py
+++ Python/#Main.py
@@ -1,1 +1,42 @@
 #Main
+
+from tkinter import *
+import tkinter as ttk
+import time
+
+root = Tk
+root.geometry("500x500")
+
+#Static Labels
+tickLabel = ttk.Label(
+    root,
+    text = "Ticks:"
+)
+
+timeLabel = ttk.Label(
+    root,
+    text = "Time:"
+)
+
+#Output Labels
+tickCount = ttk.Label(
+    root,
+    text = ""
+)
+
+timeCount = ttk.Label(
+    root,
+    text = ""
+)
+
+
+
+
+
+
+
+
+
+root.grid()
+
+root.mainloop()
